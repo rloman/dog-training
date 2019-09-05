eb25ee080fa42f52d8ca9582bce929e89322f2b2
Raymond Loman <raymond@carpago.nl>
2019-09-05 11:36:45 +0200

    Add some demo code for bounding

--- src/nl/itacademy/ocp/generics/unboundedAndUpperBoundedAndLowerBounded/AnimalDoctor.java
+++ src/nl/itacademy/ocp/generics/unboundedAndUpperBoundedAndLowerBounded/AnimalDoctor.java
@@ -5,7 +5,12 @@ import java.util.List;
 public final class AnimalDoctor {
 
 
+    // verwacht een List<Animal>
     public static void cureAnimalsUnbounded(List<Animal> animals) {
+
+        animals.add(new Cat());
+
+//        animals.add(new Object()); // vout
 
     }
 
@@ -15,6 +20,8 @@ public final class AnimalDoctor {
         // receives a List<Dog>, List<Pig> and whatever ...
 
         // unable to add  (do you know why>?)
+        Cat c = new Cat();
+//        animals.add(c); // fails
 
         // answer: since we could then send a List with Dogs
         // and add a Cat to it (which is normal expected since we are receiving an Animals list are we ...)
@@ -38,13 +45,13 @@ public final class AnimalDoctor {
         // Can we add an Object
         Object o = new Object();
 
-//        dogsOrHigherHierarchy.add(o); // of course not!
+        dogsOrHigherHierarchy.add(o); // of course not!
         // since we are having a List of Dogs. Adding a type higher in the hierarchy will fail!!!
 
         // Can we add an Animal?
         Animal a = new Animal();
 
-//        dogsOrHigherHierarchy.add(a); // again, of course not!
+        dogsOrHigherHierarchy.add(a); // again, of course not!
         // since we are having a List of Dogs. Adding a type higher in the hierarchy will fail!!!
 
 
