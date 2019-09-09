b9ced97228af520af7625a0b4b9ccf45e8705c92
Raymond Loman <raymond@carpago.nl>
2019-09-09 14:41:46 +0200

    Work in progress ...

---
+++ src/nl/itacademy/ocp/misc/App.java
@@ -0,0 +1,15 @@
+package nl.itacademy.ocp.misc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        List<Object> alles = new ArrayList();
+
+        List<String> also = new ArrayList();
+
+        
+    }
+}
