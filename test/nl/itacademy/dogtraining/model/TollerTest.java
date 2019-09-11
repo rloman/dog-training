8488fab229a594356be8c73bfaeab47d914989f6
Raymond Loman <raymond@carpago.nl>
2019-09-11 14:00:49 +0200

    Add demo code for static imports (NB: import static in the code)

--- test/nl/itacademy/dogtraining/model/TollerTest.java
+++ test/nl/itacademy/dogtraining/model/TollerTest.java
@@ -1,6 +1,6 @@
 package nl.itacademy.dogtraining.model;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TollerTest {
@@ -10,7 +10,7 @@ public class TollerTest {
         Toller toller = new Toller();
         toller.setName("Dexter");
 
-        Assert.assertEquals("Dexter", toller.getName());
+        assertEquals("Dexter", toller.getName());
     }
 
     @Test
@@ -18,7 +18,7 @@ public class TollerTest {
         Toller toller = new Toller();
         toller.setAge(7);
 
-        Assert.assertEquals(7, toller.getAge());
+        assertEquals(7, toller.getAge());
     }
 
 }
