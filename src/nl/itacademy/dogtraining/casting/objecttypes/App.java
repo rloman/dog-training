29a58dbc6ed86e04190a24aec616a3da0e4d588e
Raymond Loman <raymond@carpago.nl>
2019-09-11 09:54:45 +0200

    Add demo code for casting object types

---
+++ src/nl/itacademy/dogtraining/casting/objecttypes/App.java
@@ -0,0 +1,56 @@
+package nl.itacademy.dogtraining.casting.objecttypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        List<Animal> animals = new ArrayList<>();
+        animals.add(new Dog());
+        animals.add(new Cat());
+        animals.add(new Zebra());
+
+        demoList(animals);
+
+    }
+
+    public static void demoList(List<Animal> animals) {
+
+        for(Animal animal: animals) {
+            animal.eat();
+            if(animal instanceof Dog) {
+                Dog d = (Dog) animal;
+                d.bark();
+            }
+            else {
+                if(animal instanceof Cat) {
+                    Cat c = (Cat) animal;
+                    c.miauw();
+                }
+                else {
+                    if(animal instanceof Zebra){
+                        Zebra z = (Zebra) animal;
+                        z.yank();
+                    }
+
+                    
+                }
+            }
+        }
+
+    }
+
+    public static void tooSimple() {
+        Animal a = new Dog();
+
+        if(a instanceof Dog) {
+
+            Dog dog = (Dog) a;
+
+            dog.bark();
+
+        }
+    }
+}
---
+++ src/nl/itacademy/dogtraining/casting/objecttypes/Cat.java
@@ -0,0 +1,10 @@
+package nl.itacademy.dogtraining.casting.objecttypes;
+
+public class Cat extends Animal {
+
+    public void miauw() {
+
+        System.out.println("Miauw");
+
+    }
+}
