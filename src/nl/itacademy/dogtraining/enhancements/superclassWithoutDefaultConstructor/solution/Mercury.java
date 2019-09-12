d8a45736aab44ed942b29943844d2e9299293873
Raymond Loman <raymond@carpago.nl>
2019-09-12 11:22:27 +0200

    Work in progress ...

---
+++ src/nl/itacademy/dogtraining/enhancements/superclassWithoutDefaultConstructor/solution/Mercury.java
@@ -0,0 +1,10 @@
+package nl.itacademy.dogtraining.enhancements.superclassWithoutDefaultConstructor.solution;
+
+import java.awt.*;
+
+public class Mercury extends Planet {
+
+    public Mercury(int size, Color color) {
+        super(size, color);
+    }
+}
---
+++ src/nl/itacademy/dogtraining/enhancements/superclassWithoutDefaultConstructor/solution/Venus.java
@@ -0,0 +1,9 @@
+package nl.itacademy.dogtraining.enhancements.superclassWithoutDefaultConstructor.solution;
+
+import java.awt.*;
+
+public class Venus extends Planet {
+    public Venus(int size, Color color) {
+        super(size, color);
+    }
+}
