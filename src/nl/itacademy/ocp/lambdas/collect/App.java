91d98b8cb4573d3feb3ea55899f415a4fef4b637
Raymond Loman <raymond@carpago.nl>
2019-09-12 11:57:39 +0200

    Work in progress ...

--- src/nl/itacademy/ocp/lambdas/collect/App.java
+++ src/nl/itacademy/ocp/lambdas/collect/App.java
@@ -5,25 +5,36 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+// Credits (en mooie demo voorbeelden): => https://www.concretepage.com/java/jdk-8/java-8-stream-collect-example
 public class App {
+
+    /*
+
+     Stream<String> stream = Stream.of("w", "o", "l", "f");
+
+        //collect it to a Stringbuilder e.g.
+//    stream.collect(supplier, appender, merger)
+        // first ... create a StringBuilder
+        // second ... append the intermediate result to the Stringbuilder
+        // last ... merge all the result of the intermediate to the StringBuilder using
+        StringBuilder wordBuilder = stream.collect(StringBuilder::new,
+                StringBuilder::append,
+                StringBuilder::append);
+
+        System.out.println(wordBuilder.toString());
+     */
 
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
         numbers.addAll(Arrays.asList(0,1,1,2,3,5,8,13,21)); //54
 
-        // assignment
-        // use lambda
-        // reduce
-
-
         // Iets als dit!!!
-        /*
-        numbers.stream().collect(ArrayList::new, List::add, (left, right) -> {
+        numbers.stream().collect(ArrayList::new, List<Integer>::add, (left, right) -> {
             left.addAll(right);
 
             return left;
         });
-        */
 
         List<Integer> squares = numbers
                 .stream()
