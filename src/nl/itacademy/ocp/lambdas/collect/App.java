9ded79eb1aa74e20a6d3d710f9c03cabe2666d92
Raymond Loman <raymond@carpago.nl>
2019-09-12 15:24:16 +0200

    Add more demo for collect

--- src/nl/itacademy/ocp/lambdas/collect/App.java
+++ src/nl/itacademy/ocp/lambdas/collect/App.java
@@ -2,43 +2,56 @@ package nl.itacademy.ocp.lambdas.collect;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 // Credits (en mooie demo voorbeelden): => https://www.concretepage.com/java/jdk-8/java-8-stream-collect-example
 public class App {
 
-    /*
-
-     Stream<String> stream = Stream.of("w", "o", "l", "f");
-
-        //collect it to a Stringbuilder e.g.
-//    stream.collect(supplier, appender, merger)
-        // first ... create a StringBuilder
-        // second ... append the intermediate result to the Stringbuilder
-        // last ... merge all the result of the intermediate to the StringBuilder using
-        StringBuilder wordBuilder = stream.collect(StringBuilder::new,
-                StringBuilder::append,
-                StringBuilder::append);
-
-        System.out.println(wordBuilder.toString());
-     */
-
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
-        numbers.addAll(Arrays.asList(0,1,1,2,3,5,8,13,21)); //54
+        numbers.addAll(Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21)); //54
 
-        ArrayList<Integer>  result =  numbers.stream().collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        Bucket<Integer> result = numbers.stream().collect(Bucket::new, Bucket::add, (left, right) -> left.addAll(right));
 
-
-        System.out.println(result);
+        System.out.println("Final result:"+result);
 
         List<Integer> squares = numbers
                 .stream()
-                .map(n -> n *n ).collect(Collectors.toList());
+                .map(n -> n * n).collect(Collectors.toList());
 
         System.out.println(squares);
+    }
 
+}
+
+class Bucket<T> {
+    private List<T> content = new ArrayList<>();
+
+    public boolean add(T t) {
+
+        System.out.println("Adding "+t);
+        this.content.add(t);
+
+        return true;
+    }
+
+    public boolean addAll(Bucket<T> collection) {
+
+        System.out.println("Adding collection: "+collection);
+        this.content.addAll(collection.content);
+        System.out.println("Collection added, collection is now: => "+this.content);
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Bucket{" +
+                "content=" + content +
+                '}';
     }
 }
